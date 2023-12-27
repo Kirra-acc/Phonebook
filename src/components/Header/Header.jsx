c5c76cc526cb4aadcde849d064a778cb73a842f0
Kirra-Potiekhina <kirrapotehina@gmail.com>
2023-12-28 03:37:56 +0400

    username header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navbar } from './Navbar';
+import { useSelector } from 'react-redux';
+import { selectUserName } from 'store/auth/selectors';
 
 export const Header = () => {
+  const user = useSelector(selectUserName);
   return (
     <header>
       <Navbar />
+      {user && <h2>{user}</h2>}
     </header>
   );
 };
